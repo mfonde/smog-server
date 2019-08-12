6948dc08345c05d38aa3902797b1a5bdf8630c1f
Megan Fonde <meganfonde@gmail.com>
2019-08-12 10:45:57 -0400

    added ranking to favorites

--- controllers/favoritecontroller.js
+++ controllers/favoritecontroller.js
@@ -4,28 +4,39 @@ const sequelize = require('../db');
 const Favorite = sequelize.import('../models/favorite');
 const validateSession = require('../middleware/validate-session');
 
-router.post('/', validateSession, (req, res) => {
-    Favorite.create({
-        movieTitle: req.body.movieTitle,
-        poster: req.body.poster,
-        userId: req.user.id,
-        username: req.user.username,
-        imdbId: req.body.imdbId
-    }).then(favorite => res.status(200).json(favorite))
-    .catch(err => res.status(500).json(req.errors))
-})
-
-// router.post('/', (req, res) => {
+// router.post('/', validateSession, (req, res) => {
 //     Favorite.create({
 //         movieTitle: req.body.movieTitle,
 //         poster: req.body.poster,
+//         userId: req.user.id,
+//         username: req.user.username,
 //         imdbId: req.body.imdbId
 //     }).then(favorite => res.status(200).json(favorite))
 //     .catch(err => res.status(500).json(req.errors))
 // })
 
-router.get('/myfavorites', validateSession, (req, res) => {
-    Favorite.findAll({where: {userId: req.user.id}})
+router.post('/', (req, res) => {
+    Favorite.create({
+        movieTitle: req.body.movieTitle,
+        poster: req.body.poster,
+        imdbId: req.body.imdbId,
+        ranking: req.body.ranking
+    }).then(favorite => res.status(200).json(favorite))
+    .catch(err => res.status(500).json(req.errors))
+})
+
+// router.get('/myfavorites', validateSession, (req, res) => {
+//     Favorite.findAll({where: {userId: req.user.id}, order:[
+//         ['ranking']
+//     ]})
+//     .then(favorite => res.status(200).json(favorite))
+//     .catch(err => res.status(500).json({error: err}))
+// })
+
+router.get('/myfavorites', (req, res) => {
+    Favorite.findAll({ order:[
+        ['ranking']
+    ]})
     .then(favorite => res.status(200).json(favorite))
     .catch(err => res.status(500).json({error: err}))
 })
@@ -34,6 +45,23 @@ router.get('/username/:username', validateSession, (req, res) => {
     Favorite.findAll({where: {username: req.params.username}})
     .then(favorite => res.status(200).json(favorite))
     .catch(err => res.status(500).json({error: err}))
+})
+
+router.put('/update/:id', validateSession, (req, res) => {
+    const favorite = req.params.id;
+    const admin = req.user.admin;
+
+    if(admin == true){
+        Favorite.update(req.body.ranking, {where: {id: favorite}})
+        .then(rev => res.status(200).json(rev))
+        .catch(err => res.json({error: err}))
+
+    } else {
+        Favorite.update(req.body.ranking, {where: {id: req.params.id, userId: req.user.id}})
+        .then(rev => res.status(200).json(rev))
+        .catch(err => res.json({error: err}))
+
+    }
 })
 
 router.delete('/delete/:id', validateSession, (req, res) => {
