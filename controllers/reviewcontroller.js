5234d8e6345852b9f5e3dc17bc8e8a52a53cae7c
Owen Edwards <omedwards@outlook.com>
2019-08-20 16:05:49 -0400

    slight edits

--- controllers/reviewcontroller.js
+++ controllers/reviewcontroller.js
@@ -15,40 +15,46 @@ router.post('/', validateSession, (req, res) => {
         username: req.user.username,
         reviewRating: req.body.reviewRating,
         reviewText: req.body.reviewText,
-        imdbId: req.body.imdbId  
+        imdbId: req.body.imdbId
     })
-    .then(review => res.status(200).json(review))
-    .catch(err => res.status(500).json(req.errors))
+        .then(review => res.status(200).json(review))
+        .catch(err => res.status(500).json(req.errors))
 })
 
 router.get('/getall', (req, res) => {
-    Review.findAll({order: [
-        ['id', 'DESC']
-    ]})
-    .then(review => res.status(200).json(review))
-    .catch(err => res.status(500).json({error: err}))
+    Review.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    })
+        .then(review => res.status(200).json(review))
+        .catch(err => res.status(500).json({ error: err }))
 })
 
 router.get('/username/:username', validateSession, (req, res) => {
-    Review.findAll({where: {username: req.params.username}, order:[
-        ['id', 'DESC']
-    ]})
-    .then(review => res.status(200).json(review))
-    .catch(err => res.status(500).json({error: err}))
+    Review.findAll({
+        where: { username: req.params.username }, order: [
+            ['id', 'DESC']
+        ]
+    })
+        .then(review => res.status(200).json(review))
+        .catch(err => res.status(500).json({ error: err }))
 })
 
 router.get('/imdbid/:imdbId', (req, res) => {
-    Review.findAll({where: {imdbId: req.params.imdbId}, order: [
-        ['id', 'DESC']
-    ]})
-    .then(review => res.status(200).json(review))
-    .catch(err => res.status(500).json({error: err}))
+    Review.findAll({
+        where: { imdbId: req.params.imdbId }, order: [
+            ['id', 'DESC']
+        ]
+    })
+        .then(review => res.status(200).json(review))
+        .catch(err => res.status(500).json({ error: err }))
 })
 
 router.get('/reviewid/:id', (req, res) => {
-    Review.findAll({where: {id: req.params.id}})
-    .then(review => res.status(200).json(review))
-    .catch(err => res.json({error: err}))
+    Review.findAll({ where: { id: req.params.id } })
+        .then(review => res.status(200).json(review))
+        .catch(err => res.json({ error: err }))
 })
 
 //SYNTAX FOR MORE COMPLEX QUERIES. Also need to import a NEW version of Sequelize (not just smaller local version from './db'), and set a variable to Sqeuelize.Op. Full list of querying options at https://sequelize.org/master/manual/querying.html
@@ -67,23 +73,23 @@ router.put('/update/:id', validateSession, (req, res) => {
     const review = req.params.id;
     const admin = req.user.admin;
 
-    if(admin == true){
+    if (admin == true) {
         Review.update({
-            reviewText: req.body.reviewText, 
+            reviewText: req.body.reviewText,
             reviewRating: req.body.reviewRating
         },
-            {where: {id: review}})
-        .then(rev => res.status(200).json(rev))
-        .catch(err => res.json({error: err}))
+            { where: { id: review } })
+            .then(rev => res.status(200).json(rev))
+            .catch(err => res.json({ error: err }))
 
     } else {
         Review.update({
             reviewText: req.body.reviewText,
             reviewRating: req.body.reviewRating
-        }, 
-            {where: {id: req.params.id, userId: req.user.id}})
-        .then(rev => res.status(200).json(rev))
-        .catch(err => res.json({error: err}))
+        },
+            { where: { id: req.params.id, userId: req.user.id } })
+            .then(rev => res.status(200).json(rev))
+            .catch(err => res.json({ error: err }))
 
     }
 })
@@ -92,14 +98,14 @@ router.delete('/delete/:id', validateSession, (req, res) => {
     const review = req.params.id;
     const admin = req.user.admin;
 
-    if(admin == true){
-        Review.destroy({where: {id: review}})
-        .then(rev => res.status(200).send(`${rev} Deleted`))
-        .catch(err => res.status(500).json({error: err}))
+    if (admin == true) {
+        Review.destroy({ where: { id: review } })
+            .then(rev => res.status(200).send(`${rev} Deleted`))
+            .catch(err => res.status(500).json({ error: err }))
     } else {
-    Review.destroy({where: {id: review, userId: req.user.id}})
-    .then(rev => res.status(200).send(`${rev} Deleted`))
-    .catch(err => res.status(500).json({error: err}))
+        Review.destroy({ where: { id: review, userId: req.user.id } })
+            .then(rev => res.status(200).send(`${rev} Deleted`))
+            .catch(err => res.status(500).json({ error: err }))
     }
 })
 
